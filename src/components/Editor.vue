51deb1fcde24607d69f0686c52ba83b9b54da8bc
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2022-12-04 11:52:27 -0500

    add vue component

---
+++ src/components/Editor.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      sections: [
+        {
+          name: "WORK EXPERIENCE",
+          description: "My work experience",
+        },
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <div v-for="section in sections">
+    <h3>{{ section.name }}</h3>
+    <p>{{ section.description }}</p>
+  </div>
+</template>
