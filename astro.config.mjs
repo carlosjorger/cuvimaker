655112df098e8599d3d6f1730c3e59f94db30119
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2022-12-04 11:27:38 -0500

    add online editor page and add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue()]
+});
