b4c5f84bf14482f6758e890dbcd5ea18300465ea
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2022-12-11 17:19:41 -0500

    create component CreateSectionModal

---
+++ src/components/CreateSectionModal.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="form">
+    <header class="form-header">
+      <h3>Add a Section</h3>
+    </header>
+    <div class="form-group">
+      <input
+        type="text"
+        class="form-control"
+        v-model="tempSectionName"
+        placeholder="Section Name"
+      />
+    </div>
+    <div class="form-group">
+      <input
+        type="text"
+        class="form-control"
+        v-model="tempSectionDescription"
+        placeholder="Section Description"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CreateSectionModal",
+  data() {
+    return {
+      tempSectionName: "",
+      tempSectionDescription: "",
+      sections: [],
+    };
+  },
+  methods: {
+    addToDo() {
+      this.sections.push({
+        name: this.tempSectionName,
+        description: this.tempSectionDescription,
+      });
+    },
+  },
+};
+</script>
+<style>
+.form {
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  width: 40%;
+  padding: 2rem;
+  font-size: 1rem;
+  background-color: white;
+  border-radius: 0.5rem;
+}
+.form-header {
+  margin-bottom: 0.5rem;
+}
+.form-group {
+  margin-bottom: 2rem;
+  box-sizing: border-box;
+}
+.form-control {
+  width: 100%;
+  box-sizing: inherit;
+}
+.form-button {
+  width: 70%;
+  margin: 0 auto;
+}
+</style>
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,37 +1,22 @@
 <template>
   <section>
-    <div class="form">
-      <header class="form-header">
-        <h3>Add a Section</h3>
-      </header>
-      <div class="form-group">
-        <input
-          type="text"
-          class="form-control"
-          v-model="tempSectionName"
-          placeholder="Section Name"
-        />
-      </div>
-      <div class="form-group">
-        <input
-          type="text"
-          class="form-control"
-          v-model="tempSectionDescription"
-          placeholder="Section Description"
-        />
-      </div>
-
-      <button v-on:click="addToDo" class="form-button">Add Section</button>
-    </div>
-
+    <create-section-modal />
     <div v-for="section in sections">
-      <h3>{{ section.name }}</h3>
-      <p>{{ section.description }}</p>
+      <article>
+        <header>
+          <h3>{{ section.name }}</h3>
+          <hr />
+        </header>
+
+        <p>{{ section.description }}</p>
+      </article>
     </div>
   </section>
 </template>
 <script>
+import CreateSectionModal from "./CreateSectionModal.vue";
 export default {
+  components: { CreateSectionModal },
   data() {
     return {
       tempSectionName: "",
@@ -50,29 +35,7 @@ export default {
 };
 </script>
 <style>
-.form {
-  display: flex;
-  flex-direction: column;
-  margin: 0;
+article {
   width: 40%;
-  padding: 2rem;
-  font-size: 1rem;
-  background-color: white;
-  border-radius: 0.5rem;
-}
-.form-header {
-  margin-bottom: 0.5rem;
-}
-.form-group {
-  margin-bottom: 2rem;
-  box-sizing: border-box;
-}
-.form-control {
-  width: 100%;
-  box-sizing: inherit;
-}
-.form-button {
-  width: 70%;
-  margin: 0 auto;
 }
 </style>
