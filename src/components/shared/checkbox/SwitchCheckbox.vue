3c53516262181d3c7ae1079230111ddc1e5a5ea0
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-02-06 15:57:47 -0500

    add style to switchCheckboc component

--- src/components/shared/checkbox/SwitchCheckbox.vue
+++ src/components/shared/checkbox/SwitchCheckbox.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <input type="checkbox" v-model="value" />
+  <div class="switch-checkbox">
+    <input type="checkbox" class="input-checkbox" v-model="value" />
+    <div class="skin one"></div>
+    <div class="skin two"></div>
   </div>
 </template>
 
@@ -29,4 +31,57 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+.switch-checkbox {
+  display: grid;
+  width: 6rem;
+  height: 2rem;
+  border-radius: 45px;
+  padding: 0.5rem;
+}
+.switch-checkbox > * {
+  grid-area: 1/1;
+}
+.input-checkbox {
+  opacity: 0;
+  cursor: pointer;
+}
+
+.skin {
+  pointer-events: none;
+  border-radius: inherit;
+}
+
+.one.skin {
+  background-color: white;
+}
+
+.two.skin {
+  background-color: purple;
+
+  --mask: radial-gradient(
+    circle at 1rem 1rem,
+    rgba(0, 0, 0, 0) 0.8rem,
+    rgba(0, 0, 0, 1) 0.8rem
+  );
+  mask-image: var(--mask);
+  -webkit-mask-image: var(--mask);
+  -webkit-mask-position: 0rem 0rem;
+  mask-position: 0rem 0rem;
+  transition: all 0.5s ease;
+}
+
+.input-checkbox:checked ~ .two.skin {
+  --mask: radial-gradient(
+    circle at 1rem 1rem,
+    rgba(0, 0, 0, 0) 0.8rem,
+    rgba(0, 0, 0, 1) 0.8rem
+  );
+
+  mask-image: var(--mask);
+  -webkit-mask-image: var(--mask);
+  -webkit-mask-position: 4rem 0rem;
+  mask-position: 4rem 0rem;
+  transition: all 0.5s ease;
+}
+</style>
