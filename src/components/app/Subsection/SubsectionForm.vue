79c8aad866ead6dcce4b8bbd70452d74d81c3a9b
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-02-18 17:19:39 -0500

    add tailwind to project

--- src/components/app/Subsection/SubsectionForm.vue
+++ src/components/app/Subsection/SubsectionForm.vue
@@ -1,9 +1,14 @@
 <template>
   <input
     v-model="value"
-    class="subsection-form-control"
+    class="block p-1 w-10/10 mt-2 bg-inherit border-white text-white placeholder-gray-500 focus:outline-none"
     v-if="editing || value"
-    :class="{edit: editing}"
+    :class="{
+      ['border-solid border-b-4  pointer-events-auto shadow-xl transition-all duration-300']:
+        editing,
+      ['border-solid border-b-0  pointer-events-none shadow-none transition-all duration-300']:
+        !editing,
+    }"
     type="text" />
 </template>
 
@@ -31,38 +36,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.subsection-form-control {
-  padding: 0.1rem;
-  width: 100%;
-  margin-top: 0.5rem;
-  border: 0;
-  border-radius: 0;
-  background-color: inherit;
-  pointer-events: none;
-  transition: all 0.3s ease-in;
-  color: white;
-  border-bottom: rgba(255, 255, 255, 0) solid 0.1rem;
-  box-shadow: 0;
-}
-
-.subsection-form-control.edit {
-  border-bottom: white solid 0.1rem;
-  transition: all 0.3s ease-out;
-  pointer-events: auto;
-  box-shadow: 0px 10px 6px -6px #220b44e7;
-  font-size: 1em;
-}
-.subsection-form-control::placeholder {
-  transition: all 0.3s ease-in;
-  color: rgba(166, 166, 166, 0);
-}
-.subsection-form-control.edit::placeholder {
-  transition: all 0.3s ease-out;
-  color: var(--placeholder-color);
-}
-.subsection-form-control:focus {
-  outline: none;
-}
-</style>
