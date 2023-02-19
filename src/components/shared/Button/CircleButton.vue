4850f87562ba87baf9533115f008042dab5cc1ba
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-02-19 09:14:39 -0500

    remove CircleButton classes

--- src/components/shared/Button/CircleButton.vue
+++ src/components/shared/Button/CircleButton.vue
@@ -4,41 +4,24 @@
       backgroundColor: buttonColor,
       width: `${size}rem`,
       height: `${size}rem`,
-      padding: `${(size??0)/12}rem`
+      padding: `${(size ?? 0) / 12}rem`,
     }"
     type="button"
-    class="button-icon">
+    class="rounded-full bg-white flex relative w-12 h-12 p-1 cursor-pointer transition-all duration-300 ease-in justify-evenly items-center">
     <slot></slot>
   </button>
 </template>
 <script lang="ts">
 export default {
-  props:{
-    buttonColor:{
-      type:String
+  props: {
+    buttonColor: {
+      type: String,
     },
-    size:{
-      type:Number
-    }
+    size: {
+      type: Number,
+    },
   },
   name: "CircleButton",
   methods: {},
 };
 </script>
-<style>
-.button-icon {
-  border-radius: 50%;
-  background-color: white;
-  display: flex;
-  position: relative;
-
-  width: 3rem;
-  height: 3rem;
-  padding: 0.25rem;
-  cursor: pointer;
-  transition: all 0.3s ease-in;
-  border: var(--primary-color) solid 0.2rem;
-  justify-content: space-evenly;
-  align-items: center;
-}
-</style>
