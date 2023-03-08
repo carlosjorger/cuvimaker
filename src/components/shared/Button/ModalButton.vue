edac292fc38d68130a085738791f44481f13d24d
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-03-07 22:12:54 -0500

    change style un subsection menu

--- src/components/shared/Button/ModalButton.vue
+++ src/components/shared/Button/ModalButton.vue
@@ -1,7 +1,7 @@
 <template>
     <button
         type="submit"
-        class="mt-2 w-5/12 rounded-lg bg-white p-2 text-[var(--primary-color)] transition-colors duration-500"
+        class="mt-2 w-5/12 rounded-3xl bg-white p-2 text-lg font-extrabold text-[var(--primary-color)] transition-colors duration-500"
     >
         {{ name }}
     </button>
