73a4d860d4700ce1bf00a8fbb2d75ac98877f780
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-03-08 21:41:00 -0500

    create setting bar in section

--- src/components/app/SectionView/SectionComponent.vue
+++ src/components/app/SectionView/SectionComponent.vue
@@ -1,13 +1,16 @@
 <template>
     <article
-        class="mt-4 rounded-md border-4 border-solid border-[var(--primary-color)] bg-[#f6f4fb] p-4 shadow-xl dark:border-zinc-300 dark:bg-[#190f35]"
+        class="mt-4 rounded-md border-4 border-solid border-[var(--primary-color)] bg-[#f6f4fb] shadow-xl dark:border-zinc-300 dark:bg-[#190f35]"
+        @click="changeSetting"
     >
-        <header>
-            <h3
-                class="text-3xl font-extrabold text-[var(--primary-color)] dark:text-white"
-            >
-                {{ section?.name }}
-            </h3>
+        <div class="p-4">
+            <header>
+                <h3
+                    class="text-3xl font-extrabold text-[var(--primary-color)] dark:text-white"
+                >
+                    {{ section?.name }}
+                </h3>
+            </header>
             <hr
                 class="mt-1 h-1 rounded-md bg-[var(--primary-color)] dark:bg-zinc-300"
             />
@@ -16,19 +19,43 @@
                 :key="index"
                 :subsection="subsection"
             />
-        </header>
+        </div>
+        <div
+            class="w-full overflow-hidden bg-[var(--primary-color)] transition-all duration-500"
+            :class="{
+                ['h-10']: showSetting,
+                ['h-0']: !showSetting,
+            }"
+        >
+            <div class="flex w-20 justify-between p-2">
+                <Icon icon="mdi:trash" width="22" color="#f5f0ff" />
+                <Icon icon="material-symbols:edit" width="22" color="#f5f0ff" />
+            </div>
+        </div>
     </article>
 </template>
 <script lang="ts">
     import { Section } from '../../../models/Section';
     import SubsectionComponent from './SubsectionView/SubsectionComponent.vue';
+    import { Icon } from '@iconify/vue';
+
     export default {
         name: 'SectionComponent',
-        components: { SubsectionComponent },
+        components: { SubsectionComponent, Icon },
         props: {
             section: {
                 type: Section,
                 require: true,
+            },
+        },
+        data() {
+            return {
+                showSetting: false,
+            };
+        },
+        methods: {
+            changeSetting() {
+                this.showSetting = !this.showSetting;
             },
         },
     };
