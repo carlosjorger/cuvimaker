a778f77552aaab0d45f57edef5f43d6c9259cc77
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-03-15 22:07:53 -0400

    add date range view

--- src/components/app/SectionView/SubsectionView/SubsectionComponent.vue
+++ src/components/app/SectionView/SubsectionView/SubsectionComponent.vue
@@ -1,13 +1,41 @@
 <template>
-    <h4 class="mt-2 font-semibold">{{ subsection?.title }}</h4>
-    <h5 class="mt-2 font-extrabold">{{ subsection?.text }}</h5>
+    <h4 class="mt-2 text-xl font-semibold">{{ subsection?.title }}</h4>
+    <h5 class="mt-2 text-xl font-extrabold">{{ subsection?.text }}</h5>
+    <div
+        class="flex flex-row items-center"
+        v-if="subsection?.subsectionTimeInterval != null"
+    >
+        <Icon icon="material-symbols:date-range" width="25" color="white" />
+        &nbsp;
+        <div>
+            {{
+                subsection?.subsectionTimeInterval?.dateFrom?.toLocaleString(
+                    'en-US',
+                    { month: 'long' }
+                )
+            }}
+            {{ subsection?.subsectionTimeInterval?.dateFrom?.getDate() }}
+        </div>
+        &nbsp;-&nbsp;
+        <div>
+            {{
+                subsection?.subsectionTimeInterval?.dateTo?.toLocaleString(
+                    'en-US',
+                    { month: 'long' }
+                )
+            }}
+            {{ subsection?.subsectionTimeInterval?.dateTo?.getDate() }}
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
     import { Subsection } from '../../../../models/Subsection';
+    import { Icon } from '@iconify/vue';
 
     export default {
         name: 'SubsectionComponent',
+        components: { Icon },
         props: {
             subsection: {
                 type: Subsection,
