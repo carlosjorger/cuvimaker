ca838b26224a1923b90f18a9d8aa11f36c8005c5
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-07-01 21:32:41 -0400

    Add new TODO

--- src/components/CVEditor.vue
+++ src/components/CVEditor.vue
@@ -1,6 +1,7 @@
 <!-- TODO: Save the pdf -->
 <!-- TODO: Make a better positioning for the contacts -->
 <!-- TODO: Save the cv in the sotarage -->
+<!-- TODO: center sections in a smaller view -->
 <template>
     <section>
         <IntroductionSection
