8b0cf0c1bbda5f97bd33c4b71917070dcd5a030d
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-07-09 20:29:06 -0400

    ✨ feat: Preview Section
    Add Section Preview component

--- src/components/app/Editor/PreviewResume.vue
+++ src/components/app/Editor/PreviewResume.vue
@@ -2,6 +2,15 @@
 	<AppearFadePanelTransition>
 		<div v-show="canShowPreviewResume" class="p-5 max-lg:p-3 max-md:p-2">
 			<PreviewIntroduction :introduction="resume.introduction" />
+			<div
+				class="mt-5 grid grid-cols-2 gap-3 p-1 text-base max-md:grid-cols-1"
+			>
+				<PreviewSection
+					v-for="section in resume.sections"
+					:section="section"
+					:key="section.name"
+				/>
+			</div>
 		</div>
 	</AppearFadePanelTransition>
 </template>
@@ -10,9 +19,13 @@
 	import { Resume } from '../../../models/Resume';
 	import AppearFadePanelTransition from '../../shared/Transition/AppearFadePanelTransition.vue';
 	import PreviewIntroduction from './PreviewIntroduction.vue';
-
+	import PreviewSection from './PreviewSection.vue';
 	export default {
-		components: { AppearFadePanelTransition, PreviewIntroduction },
+		components: {
+			AppearFadePanelTransition,
+			PreviewIntroduction,
+			PreviewSection,
+		},
 		props: {
 			resume: {
 				type: Resume,
---
+++ src/components/app/Editor/PreviewSection.vue
@@ -0,0 +1,23 @@
+<template>
+	<section class="m-2">
+		<header>
+			<h3 class="text-xl font-extrabold text-primary dark:text-white">
+				{{ section?.name }}
+			</h3>
+		</header>
+		<hr class="mt-1 h-1 rounded-md bg-primary dark:bg-zinc-300" />
+	</section>
+</template>
+
+<script lang="ts">
+	import { Section } from '../../../models/Section';
+
+	export default {
+		props: {
+			section: {
+				type: Section,
+				required: true,
+			},
+		},
+	};
+</script>
