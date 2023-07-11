17dbd6681f96d35977894db85e8e22bbbb1d5cb9
CarlosJorgeR <rodriguezcuelloc@gmail.com>
2023-07-11 18:17:07 -0400

    🐞 fix: hasElementList
    update hasElementList in mounted

--- src/components/app/List/SubsectionListSection.vue
+++ src/components/app/List/SubsectionListSection.vue
@@ -1,37 +1,45 @@
 <template>
-    <div class="mx-2 mt-2">
-        <SwitchCheckbox
-            v-if="editing"
-            v-model="hasElementList"
-            :title="'Add a List'"
-        />
-        <subsection-elements v-if="hasElementList" />
-    </div>
+	<div class="mx-2 mt-2">
+		<SwitchCheckbox
+			v-if="editing"
+			v-model="hasElementList"
+			:title="'Add a List'"
+		/>
+		<subsection-elements v-if="hasElementList" />
+	</div>
 </template>
 
 <script lang="ts">
-    import { inject } from 'vue';
-    import { Subsection } from '../../../models/Subsection';
-    import SwitchCheckbox from '../../shared/checkbox/SwitchCheckbox.vue';
-    import SubsectionElements from './SubsectionElements.vue';
-    export default {
-        name: 'SubsectionListSection',
-        components: { SwitchCheckbox, SubsectionElements },
+	import { inject } from 'vue';
+	import { Subsection } from '../../../models/Subsection';
+	import SwitchCheckbox from '../../shared/checkbox/SwitchCheckbox.vue';
+	import SubsectionElements from './SubsectionElements.vue';
+	export default {
+		name: 'SubsectionListSection',
+		components: { SwitchCheckbox, SubsectionElements },
 
-        data() {
-            return {
-                editing: inject('editing', false),
-                hasElementList: false,
-                subsection: inject('subsection', new Subsection()),
-                showModal: inject('showModal', false),
-            };
-        },
-        watch: {
-            subsection(subsection: Subsection) {
-                this.hasElementList = subsection.elements.length > 0;
-            },
-        },
-    };
+		data() {
+			return {
+				editing: inject('editing', false),
+				hasElementList: false,
+				subsection: inject('subsection', new Subsection()),
+				showModal: inject('showModal', false),
+			};
+		},
+		mounted() {
+			this.hasElementList = this.updatedHasElementList(this.subsection);
+		},
+		methods: {
+			updatedHasElementList(subsection: Subsection) {
+				return (this.hasElementList = subsection.elements.length > 0);
+			},
+		},
+		watch: {
+			subsection(subsection: Subsection) {
+				this.hasElementList = this.updatedHasElementList(subsection);
+			},
+		},
+	};
 </script>
 
 <style scoped></style>
