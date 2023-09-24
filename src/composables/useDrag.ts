bbe4c1bb2b43805a381edb1be0b1dade4cfb7fab
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-09-24 11:39:04 -0400

    ✨ feat: create composables

---
+++ src/composables/useDrag.ts
@@ -0,0 +1,31 @@
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+//TODO: use this composable
+export function useEventListener<T>(collection: Ref<T[]>, save: () => void) {
+	const markedSection = ref(-1);
+	const onDragEnter = (index: number) => {
+		markedSection.value = index;
+	};
+	const onDrop = (event: DragEvent, element: T, index: number) => {
+		const eventDataTransfer = event.dataTransfer;
+		if (eventDataTransfer) {
+			const draggedSectionName = eventDataTransfer.getData('itemID');
+			const sourceIndex = Number.parseInt(draggedSectionName);
+			const sourceSection = collection[sourceIndex];
+			collection[sourceIndex] = element;
+			collection[index] = sourceSection;
+			this.markedSection = -1;
+			save();
+		}
+	};
+	const startDrag = (event: DragEvent, index: number) => {
+		const eventDataTransfer = event.dataTransfer;
+		if (eventDataTransfer) {
+			eventDataTransfer.dropEffect = 'move';
+			eventDataTransfer.effectAllowed = 'move';
+			eventDataTransfer.setData('itemID', index.toString());
+		}
+		(event.target as HTMLElement).style.opacity = '1';
+	};
+	return { onDragEnter, onDrop, startDrag, markedSection };
+}
