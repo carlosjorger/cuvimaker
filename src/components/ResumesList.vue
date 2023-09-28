be7a982b4c1799f0c1e720d8ccf9c35f3cdabb44
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-09-28 17:23:47 -0400

    ✨ feat: Make responsible resume list

--- src/components/ResumesList.vue
+++ src/components/ResumesList.vue
@@ -1,41 +1,40 @@
 <!-- TODO: Add clear resume button -->
-<!-- TODO: Make responsible -->
 <!-- TODO: improve the resume design list -->
 <template>
-	<div class="m-4">
-		<h2
-			class="mx-20 my-3 font-montserrat text-4xl font-extrabold text-zinc-50"
-		>
+	<div class="m-4 mx-20 max-md:mx-10 max-sm:mx-auto max-sm:w-11/12">
+		<h2 class="my-3 font-montserrat text-4xl font-extrabold text-zinc-50">
 			Resumes
 		</h2>
 		<ul>
 			<li v-for="path in paths" :key="path.params.id">
 				<a :href="`/editor/${path.params.id}`">
 					<div
-						v-if="
-							getResumeName(path.params.id)?.introduction.name &&
-							getResumeName(path.params.id)?.introduction
-								.profetion
-						"
-						class="mx-20 my-3 w-2/3 rounded-lg bg-[#f5f0ff] p-7 font-extrabold transition-all dark:text-dark-primary max-md:w-5/6 max-sm:w-11/12"
+						class="my-3 w-2/3 rounded-lg bg-[#f5f0ff] p-7 font-extrabold transition-all dark:text-dark-primary max-md:w-5/6 max-sm:w-full"
 					>
-						<div class="text-lg">
-							{{
-								getResumeName(path.params.id)?.introduction.name
-							}}
-						</div>
-						<div>
-							{{
+						<div
+							v-if="
+								getResumeName(path.params.id)?.introduction
+									.name &&
 								getResumeName(path.params.id)?.introduction
 									.profetion
-							}}
+							"
+						>
+							<div class="text-lg">
+								{{
+									getResumeName(path.params.id)?.introduction
+										.name
+								}}
+							</div>
+							<div>
+								{{
+									getResumeName(path.params.id)?.introduction
+										.profetion
+								}}
+							</div>
 						</div>
-					</div>
-					<div
-						v-else
-						class="mx-20 my-3 w-2/3 rounded-lg bg-[#f5f0ff] p-7 font-blackOpsOne font-extrabold transition-all dark:text-dark-primary max-md:w-5/6 max-sm:w-11/12"
-					>
-						<div class="text-2xl">EMPTY</div>
+						<div v-else class="font-blackOpsOne text-2xl">
+							EMPTY
+						</div>
 					</div>
 				</a>
 			</li>
