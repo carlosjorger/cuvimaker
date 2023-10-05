848d9b196e152ec65dbba98bc9bb812a8932eb15
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-10-05 19:26:11 -0400

    📃 docs: Add TODO

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 /*eslint-env node*/
+// TODO: Add daisy UI
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,astro}'],
 	darkMode: 'class',
