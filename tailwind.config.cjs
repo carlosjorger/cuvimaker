0220867d563c13d9aeb5f90489ce3d9bc4d1755a
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-10-18 10:46:34 -0400

    ✨ feat: add initial animation to CardImage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,6 +22,33 @@ module.exports = {
 				form: 'border-bottom, box-shadow',
 			},
 			translate: ['dark'],
+			keyframes: {
+				cardImageIn: {
+					'0%': {
+						transform: 'rotateX(15deg) rotateY(-15deg)',
+						'box-shadow': '15px 15px 45px var(--tw-shadow-color)',
+					},
+					'25%': {
+						transform: 'rotateX(-11deg) rotateY(-5deg)',
+						'box-shadow': '5px -11px 38px var(--tw-shadow-color)',
+					},
+					'50%': {
+						transform: 'rotateX(4deg) rotateY(7deg)',
+						'box-shadow': '-7px 4px 35.5px var(--tw-shadow-color)',
+					},
+					'75%': {
+						transform: 'rotateX(2deg) rotateY(-3deg)',
+						'box-shadow': '3px 2px 33px var(--tw-shadow-color)',
+					},
+					'100%': {
+						transform: 'rotateX(0deg) rotateY(0deg)',
+						'box-shadow': '0px 0px 30px var(--tw-shadow-color)',
+					},
+				},
+			},
+			animation: {
+				cardImageIn: 'cardImageIn 2s ease-in-out',
+			},
 		},
 		fontFamily: {
 			montserrat: ['Montserrat', 'ui-sans-serif'],
