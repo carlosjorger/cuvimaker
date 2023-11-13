8f2b49651175f015dedfb8a7803ddebde89edcad
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-11-13 12:01:40 -0500

    🐞 fix: padding resume preview

--- src/components/app/Preview/PreviewResume.vue
+++ src/components/app/Preview/PreviewResume.vue
@@ -1,7 +1,7 @@
 <template>
 	<SubsectionAlign>
 		<div
-			class="rounded-md border-primary bg-base-300 p-3 shadow-xl transition-colors duration-700 max-lg:p-2 max-md:p-1"
+			class="rounded-md border-primary bg-base-300 p-4 shadow-xl transition-colors duration-700 max-lg:p-3 max-md:p-2"
 		>
 			<PreviewIntroduction :introduction="resume.introduction" />
 			<div
