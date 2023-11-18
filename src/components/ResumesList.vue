9d641f6f55bd22e2a64e19713951b114bb64a5b3
Carlos Jorge Rodriguez <rodriguezcuelloc@gmail.com>
2023-11-18 13:10:43 -0500

    ✨ feat: add skeleton to ResumesList

--- src/components/ResumesList.vue
+++ src/components/ResumesList.vue
@@ -1,49 +1,68 @@
 <template>
 	<div class="m-2 rounded-lg bg-base-100">
-		<ul
-			class="my-5 ml-20 max-h-[calc(100vh-16rem)] min-h-[calc(100vh-12.5rem)] overflow-auto overflow-x-hidden rounded-lg bg-base-100 pt-2 focus:text-neutral-focus max-md:mx-10 max-md:w-full max-sm:mx-0"
-		>
-			<li v-for="path in paths" :key="path.params.id">
-				<a :href="`/editor/${path.params.id}`">
-					<div
-						class="card my-3 ml-12 w-5/6 rounded-lg bg-base-200 p-7 font-extrabold shadow-2xl shadow-warning-content transition-all duration-200 hover:translate-x-6 hover:bg-base-300 dark:shadow-success-content max-md:ml-8 max-md:w-11/12 max-sm:mx-auto max-sm:w-11/12 max-sm:hover:translate-x-2"
-					>
+		<AppearFadeTransition>
+			<ul
+				v-if="loading"
+				class="my-5 ml-20 max-h-[calc(100vh-16rem)] min-h-[calc(100vh-12.5rem)] overflow-auto overflow-x-hidden rounded-lg bg-base-100 pt-2 focus:text-neutral-focus max-md:mx-10 max-md:w-full max-sm:mx-0"
+			>
+				<li
+					v-for="skeletonIndex in [1, 2, 3, 4, 5]"
+					:key="skeletonIndex"
+					class="skeleton my-3 ml-12 h-24 w-5/6 rounded-lg p-7 max-md:ml-8 max-md:w-11/12 max-sm:mx-auto max-sm:w-11/12"
+				></li>
+				<li
+					class="skeleton my-3 ml-12 h-24 w-5/6 rounded-lg p-7 max-md:ml-8 max-md:w-11/12 max-sm:mx-auto max-sm:w-11/12"
+				></li>
+			</ul>
+			<ul
+				v-else
+				class="my-5 ml-20 max-h-[calc(100vh-16rem)] min-h-[calc(100vh-12.5rem)] overflow-auto overflow-x-hidden rounded-lg bg-base-100 pt-2 focus:text-neutral-focus max-md:mx-10 max-md:w-full max-sm:mx-0"
+			>
+				<li v-for="path in paths" :key="path.params.id">
+					<a :href="`/editor/${path.params.id}`">
 						<div
-							class="flex items-center justify-between"
-							v-if="
-								getResumeName(path.params.id)?.introduction
-									.name &&
-								getResumeName(path.params.id)?.introduction
-									.profetion
-							"
+							class="card my-3 ml-12 w-5/6 rounded-lg bg-base-200 p-7 font-extrabold shadow-2xl shadow-warning-content transition-all duration-200 hover:translate-x-6 hover:bg-base-300 dark:shadow-success-content max-md:ml-8 max-md:w-11/12 max-sm:mx-auto max-sm:w-11/12 max-sm:hover:translate-x-2"
 						>
-							<hgroup>
-								<h3 class="text-lg">
-									{{
-										getResumeName(path.params.id)
-											?.introduction.name
-									}}
-								</h3>
-								<h4>
-									{{
-										getResumeName(path.params.id)
-											?.introduction.profetion
-									}}
-								</h4>
-							</hgroup>
-							<CircleButtonWithIcon
-								icon="ic:baseline-delete"
-								text="Clear Resume"
-								@click.prevent="
-									tryToClearResume($event, path.params.id)
+							<div
+								class="flex items-center justify-between"
+								v-if="
+									getResumeName(path.params.id)?.introduction
+										.name &&
+									getResumeName(path.params.id)?.introduction
+										.profetion
 								"
-							/>
+							>
+								<hgroup>
+									<h3 class="text-lg">
+										{{
+											getResumeName(path.params.id)
+												?.introduction.name
+										}}
+									</h3>
+									<h4>
+										{{
+											getResumeName(path.params.id)
+												?.introduction.profetion
+										}}
+									</h4>
+								</hgroup>
+								<CircleButtonWithIcon
+									icon="ic:baseline-delete"
+									text="Clear Resume"
+									@click.prevent="
+										tryToClearResume($event, path.params.id)
+									"
+								/>
+							</div>
+							<h3 v-else class="font-blackOpsOne text-2xl">
+								EMPTY
+							</h3>
 						</div>
-						<h3 v-else class="font-blackOpsOne text-2xl">EMPTY</h3>
-					</div>
-				</a>
-			</li>
-		</ul>
+					</a>
+				</li>
+			</ul>
+		</AppearFadeTransition>
+
 		<ConfirmationModal
 			id="delete_resume_modal"
 			:entityToDelete="'resume'"
@@ -60,12 +79,14 @@
 	import ConfirmationModal from './shared/Modal/ConfirmationModal.vue';
 	import { onMounted, ref } from 'vue';
 	import { useOpenModal } from './../composables/useOpenModal';
+	import AppearFadeTransition from './shared/Transition/AppearFadeTransition.vue';
 
 	const { onShowModal } = useOpenModal('delete_resume_modal');
 	let paths = ref([] as ResumePathsType[]);
 	const indexOfElementToDelete = ref('');
+	const loading = ref(true);
 	onMounted(() => {
-		paths.value = getResumePaths();
+		loadPahts();
 	});
 	const getResumeName = (id: string) => {
 		return getResume(id);
@@ -79,8 +100,13 @@
 
 	const clear = () => {
 		clearResume(indexOfElementToDelete.value);
-		paths.value = getResumePaths();
+		loadPahts();
 		return false;
+	};
+	const loadPahts = () => {
+		loading.value = true;
+		paths.value = getResumePaths();
+		loading.value = false;
 	};
 </script>
 
